a99eceb961a1a47e42d41ba2cfcdadeb8d69c5b0
Edgaras <eddygrandas@gmail.com>
2018-10-17 00:57:21 +0300

    ketvirtas kauliukas

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Pirmas</title>
-    <style>
+<!--    <style>
         .class-1{
             font-size: 40px;
             color: brown;
@@ -38,6 +38,52 @@
     
     <p class="<?php print 'class-'.rand(1,4);?>">
           Nemoka NetBeans Lorem ipsum, arba aš nemoku :D
-    </p>
+    </p>-->
+    <style>
+        .langas{
+            max-width: 1200px;
+            margin: 0 auto;
+            background-color: lightsteelblue;
+        }
+        .langas h1{
+            font-size: 40px;
+            color: gold;
+            text-align: center;
+            
+        }
+        .mygtukas{
+            display: flex;
+            margin: 0 auto;
+            width: 150px;
+            height: 50px;
+            border: 1px solid green;
+            border-radius: 25%;
+            font-size: 26px;
+        }
+        .kauliukas{
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            
+        }
+    </style>
+        
+   </head>
+   
+   <body class="langas">
+       <h1 class="langas h1">
+           Spausk mygtuką! Čia tik vienas mygtukas!
+       </h1>
+       <div class="kauliukas">
+           
+           <img src="img/dice<?php print rand(1,6);?>.svg">
+       </div>
+       <div>
+        <form>
+        <input class="mygtukas" type="button" onClick="history.go(0)" value="Ridenti">
+        </form>
+              
+       </div>
+           
   </body>
 </html>
