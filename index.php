88d3cc5cf3d6d7be5c627f96a3e4418d8c36ce68
Edgaras <eddygrandas@gmail.com>
2018-10-17 00:00:24 +0300

    trecias kaip turi but

--- index.php
+++ index.php
@@ -5,22 +5,39 @@
     <title>Pirmas</title>
     <style>
         .class-1{
-            font-size: 20px;
+            font-size: 40px;
             color: brown;
+            background-color: yellow;
         }
         .class-2{
-            font-size: 40px;
+            font-size: 80px;
             color: orange;
+            background-color: lightgrey;
         }
         .class-3{
-            font-size: 60px;
+            font-size: 120px;
             color: greenyellow;
+            background-color: lightskyblue;
         }
+        .class-4{
+            font-size: 70px;
+            color: yellow;
+            background-color: lightpink;
+        }
+        
         </style>
   </head>
    <body>
-      <p class="class-<?php print rand(1,3);?>">
-          Lorem ipsum
+      <p class="<?php print 'class-'.rand(1,4);?>">
+          Nemoka NetBeans Lorem ipsum, arba aš nemoku :D
+    </p>
+    
+    <p class="<?php print 'class-'.rand(1,4);?>">
+          Nemoka NetBeans Lorem ipsum, arba aš nemoku :D
+    </p>
+    
+    <p class="<?php print 'class-'.rand(1,4);?>">
+          Nemoka NetBeans Lorem ipsum, arba aš nemoku :D
     </p>
   </body>
 </html>
