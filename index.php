1c56eb67b89adb61252041e6ce3069c7e549f673
Edgaras <eddygrandas@gmail.com>
2018-10-16 23:49:29 +0300

    antras veikia

--- index.php
+++ index.php
@@ -3,13 +3,24 @@
   <head>
     <meta charset="utf-8">
     <title>Pirmas</title>
+    <style>
+        .class-1{
+            font-size: 20px;
+            color: brown;
+        }
+        .class-2{
+            font-size: 40px;
+            color: orange;
+        }
+        .class-3{
+            font-size: 60px;
+            color: greenyellow;
+        }
+        </style>
   </head>
-  <body>
-      Veikia
-    <?php
-    print 'Veikia...';
-    print '...ar tikrai?';
-    print 'TAIP!';
-    ?>
+   <body>
+      <p class="class-<?php print rand(1,3);?>">
+          Lorem ipsum
+    </p>
   </body>
 </html>
