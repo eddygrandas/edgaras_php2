463c3dc55c9937c27d79fc39f071356a2e8a9333
Edgaras <eddygrandas@gmail.com>
2018-10-16 23:34:54 +0300

    pirmas

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pirmas</title>
+  </head>
+  <body>
+      Veikia
+    <?php
+    print 'Veikia...';
+    print '...ar tikrai?';
+    print 'TAIP!';
+    ?>
+  </body>
+</html>
