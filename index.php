f0ba20bbdcae70d01949032af65834db03434d36
Edgaras <eddygrandas@gmail.com>
2018-10-17 13:44:12 +0300

    pinigai

--- index.php
+++ index.php
@@ -3,25 +3,18 @@
     <head>
         <meta charset="utf-8">
         <title>Pirmas</title>
-   
-
     </head>
-<?php 
-$pic="img/dice6.svg";
-$link='http://www.technologijos.lt/';
-?>
+    <?php
+    $mano_pinigai = 1000;
+    $spent_pm = 600;
+    $earned_pm = 800;
+    $unknown_pm = rand(100, 500);
+    $months = 24;
+    $wallet_forecast = $mano_pinigai - (24 * ($earned_pm - $spent_pm - $unknown_pm));
+    ?>
     <body>
         <p>
-            <img src=<?php print $pic;?>>
-            <img src=<?php print $pic;?>>
-            <img src=<?php print $pic;?>>
-            <img src=<?php print $pic;?>>
-            
-            <a href="<?php print $link ?>">Spausti čia 1</a>
-            <a href="<?php print $link ?>">Spausti čia 2</a>
-            <a href="<?php print $link ?>">Spausti čia 3</a>
-            <a href="<?php print $link ?>">Spausti čia 4</a>
-            
+            <?php print 'Po ' . $months . ' (' . date("Y m d", strtotime('+' . $months . 'month')) . ')' . ', tikėtina turėsiu ' . $wallet_forecast . ' pinigų'; ?>
         </p>
     </body>
 </html>
