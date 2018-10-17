95448747228a55032e7b387d4545d274c0f8cd88
Edgaras <eddygrandas@gmail.com>
2018-10-17 14:25:52 +0300

    praejo dienu nuo liepos pirmos

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Pirmas</title>
-    </head>
-    <?php
-    $mano_pinigai = 1000;
-    $spent_pm = 600;
-    $earned_pm = 800;
-    $unknown_pm = rand(100, 500);
-    $months = 24;
-    $wallet_forecast = $mano_pinigai - $months * ($earned_pm - $spent_pm - $unknown_pm);
-    ?>
-    <body>
-        <p>
-            <?php print 'Po ' . $months . ' mėnesių (' . date("Y m d", strtotime('+' . $months . 'month')) . ')' . ', tikėtina turėsiu ' . $wallet_forecast . ' pinigų'; ?>
-        </p>
-    </body>
-</html>
+<?php
+$liepa_d=30;
+$rugpjutis_d=31;
+$rugsejis_d=30;
+$viso=$liepa_d+$rugpjutis_d+$rugsejis_d;
+$kiek=$viso + date("d");
+
+print $kiek;
