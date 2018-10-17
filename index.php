fb69b71cb9690bc16e016e5bac828a1277cbb34c
Edgaras <eddygrandas@gmail.com>
2018-10-17 13:15:56 +0300

    kintamieji 2

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
-        <?php 
-        $string='string';
-        $integer=5;
-        $float=1.569;
-        $boolean=true;
-        $null=NULL;
-        print("Stringas - $string <br>Integer - $integer <br>Float - $float <br>Boolean - $boolean <br>Null - $null");
-        ?>
-  
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Pirmas</title>
+   
+
+    </head>
+<?php 
+$pic="img/dice6.svg";
+$link='http://www.technologijos.lt/';
+?>
+    <body>
+        <p>
+            <img src=<?php print $pic;?>>
+            <img src=<?php print $pic;?>>
+            <img src=<?php print $pic;?>>
+            <img src=<?php print $pic;?>>
+            
+            <a href="<?php print $link ?>">Spausti čia 1</a>
+            <a href="<?php print $link ?>">Spausti čia 2</a>
+            <a href="<?php print $link ?>">Spausti čia 3</a>
+            <a href="<?php print $link ?>">Spausti čia 4</a>
+            
+        </p>
+    </body>
+</html>
