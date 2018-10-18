56303d40fbbf288633ec688ec13a6933bdfbb8c8
Edgaras <eddygrandas@gmail.com>
2018-10-18 13:40:04 +0300

    kuras2

--- index.php
+++ index.php
@@ -4,17 +4,28 @@ $fuel_100km = 7.5;
 $fuel_price = 1.3;
 $kaina = round($fuel_price / $fuel_100km * $distance, 2);
 $litrai = $distance / 100 * $fuel_100km;
+$my_money = 100;
+$pirmyn = " galiu";
+$stop = " negaliu";
+if ($my_money <= $kaina) {
+    $kelione = $stop;
+} else {
+    $kelione = $pirmyn;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
         <title>kilometrai</title>
-
     </head>
     <body >
         <p>
-            <?php print "Nuvažiavus " . $distance . " km, mašina sunaudos " . $litrai . " kuro. Kaina: " . $kaina . " pinigų"; ?>
+            <?php
+            print "Nuvažiavus " . $distance . " km, mašina sunaudos "
+                    . $litrai . " kuro. Kaina: " . $kaina . " Išvada: Aš sau tai"
+                    . $kelione . " leisti.";
+            ?>
         </p>
     </body>
 </html>
