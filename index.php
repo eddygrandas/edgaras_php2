a1fd5a6972f417876a9dfaec82d2960880905ec1
Edgaras <eddygrandas@gmail.com>
2018-10-18 13:05:34 +0300

    kuras

--- index.php
+++ index.php
@@ -1,57 +1,21 @@
 <?php
-$sunny = 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-clear-icon.png';
-$raining = 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-showers-icon.png';
-$cloudy = 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-many-clouds-icon.png';
-$sunrain = 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-showers-day-icon.png';
-$sun = rand(0, 1);
-$rain = rand(0, 1);
-$klase = "";
-
-if ($sun && $rain) {
-    $klase = "saulelyja";
-}
-if ($sun && !$rain) {
-    $klase = "sauleta";
-}
-if (!$sun && $rain) {
-    $klase = "lietinga";
-}
-if (!$sun && !$rain) {
-    $klase = "debesuota";
-}
+$distance = rand(500, 1000);
+$fuel_100km = 7.5;
+$fuel_price = 1.3;
+$kaina = round($fuel_price / $fuel_100km * $distance, 2);
+$litrai = $distance / 100 * $fuel_100km;
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Orai</title>
-        <style>
-            .sauleta{
-                height: 128px;
-                width: 128px;
-                background-image: url("<?php print $sunny ?>");  
-            }
-            .lietinga{
-                height: 128px;
-                width: 128px;
-                background-image: url("<?php print $raining ?>");  
-            }
-            .saulelyja{
-                height: 128px;
-                width: 128px;
-                background-image: url("<?php print $sunrain ?>");  
-            }
-            .debesuota{
-                height: 128px;
-                width: 128px;
-                background-image: url("<?php print $cloudy ?>");  
-            }
-        </style>
+        <title>kilometrai</title>
+
     </head>
     <body >
-        <div class="<?php print $klase; ?>">
-            <?php echo "$sun $rain"; ?>
-        </div>
+        <p>
+            <?php print "Nuvažiavus " . $distance . " km, mašina sunaudos " . $litrai . " kuro. Kaina: " . $kaina . " pinigų"; ?>
+        </p>
     </body>
 </html>
 
